dfaca1a723dad0f1111a682eac5c9dbcbff59b2d
antt996 <alan@qualmark.io>
2022-02-14 15:12:24 -0400

    Importando Apps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App2';
+import App1 from './App';
+import App2 from './App2';
 import App3 from './Contrataunheroe.jsx';
 import reportWebVitals from './reportWebVitals';
 
